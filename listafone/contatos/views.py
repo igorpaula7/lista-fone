1342af0e59cd47a5226ae2a4bf066f8728b3c1ff
Ricardo Fernandes <tr0v40.r1c@gmail.com>
2025-08-25 23:13:00 -0300

    refact templates and added list template and view. Created custom tags

--- listafone/contatos/views.py
+++ listafone/contatos/views.py
@@ -4,16 +4,17 @@ from django.views.generic.edit import FormView
 from django.db import transaction
 from contatos.forms import ContatoForm
 from contatos.models import Contato
+from django.views.generic.list import ListView
 
 
 class ContatoFormView(FormView):
-    template_name = "criar_contato.html"
+    template_name = "base_form.html"
     form_class = ContatoForm
     success_url = reverse_lazy("criar_contato")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = "Novo Contato"
+        context["title"] = "Cadastrar Contato"
         return context
 
     # Esse decorator garante que a transação seja feita de forma atomica
@@ -24,10 +25,8 @@ class ContatoFormView(FormView):
         try:
             contato = form.save(commit=False)
             contato.save()
-
             messages.success(self.request, "Contato salvo com sucesso!")
             return super().form_valid(form)
-
         except Exception as e:
             messages.error(self.request, f"Erro ao salvar o contato: {str(e)}")
             return self.form_invalid(form)
@@ -36,4 +35,10 @@ class ContatoFormView(FormView):
         messages.error(self.request, "Por favor, corrija os erros abaixo.")
         return super().form_invalid(form)
 
-    success_url = "/criar_contato/"
+    # success_url = "/criar_contato/"
+
+
+class ContatoListView(ListView):
+    template_name = "lista.html"
+    model = Contato
+    context_object_name = "dados"
